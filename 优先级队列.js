a415f10c0bc214ddb3533c65d48f1e1fda6944e2
MING0604 <1206710179@qq.com>
2020-10-12 20:06:36 +0800

    优先级对垒

---
+++ 优先级队列.js
@@ -0,0 +1,38 @@
+// 封装优先级队列
+function PriorityQueue() {
+    // 在PriorityQueue中新建了一个类
+    function QueueElement(element,priority) {
+        this.element = element
+        this.priority = priority
+    }
+
+    // 封装属性  
+    let item = []
+
+    // 实现插入方法
+    PriorityQueue.prototype.enqueue = function (element, priority) {
+        // 1.创建QueueElement对象
+        let queueElement = new QueueElement(element, priority)
+
+        // 2.判断队列是否为空
+        if(this.items.length == 0) {
+            this.items.push(queueElement)
+        }else{
+            let added = false
+            for(let i=0;i<this.items.length;i++){
+                if(queueElement.priority < this.items[i].priority){
+                    this.items.splice(i, 0, queueElement)
+                    added = true
+                    break
+                }
+            }
+
+            if(!added){
+                this.items.push(queueElement)
+            }
+        }
+    }
+
+}
+// 测试代码
+let  pq = new PriorityQueue()
